b24fe267eb8b6c6067af0698988abead08b6a027
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2021-12-28 12:22:39 +0530

    gatsby

---
+++ src/component/Intro.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Profile from "../images/profile.svg";
+
+export default function Intro() {
+  return (
+    <div>
+      <div className="card col-10 mx-auto mt-5 border-0 zoom">
+        <div className="card-body bg-theme border-rounded shadow-lg">
+          <div className="row row-cols-3 row-cols-md-4">
+            <div className="col-md-4 col-12 border-rounded d-flex align-items-center no-gutters">
+              <img src={Profile} alt="" className="mx-auto" height="310px" />
+            </div>
+            <div className="col-md-8 col-lg-8 col-12 border-rounded mx-auto text-dark text-center">
+              <div className="mt-4">
+                <h3 className="card-title">Jignesh Baria</h3>
+                <div className="card-text">Software Developer</div>
+                <div className="mx-auto mt-3">
+                  <a
+                    className="navbar-brand text-danger zoom1"
+                    href="mailto:jigneshbaria1777@gmail.com"
+                    target="_blank"
+                  >
+                    <i alt="Mail logo" class="bi bi-envelope" />
+                  </a>
+                  <a
+                    className="navbar-brand text-primary zoom"
+                    href="https://www.linkedin.com/in/jignesh-baria/"
+                    target="_blank"
+                  >
+                    <i alt="Linkedin logo" class="bi bi-linkedin" />
+                  </a>
+                  <a
+                    className="navbar-brand text-dark zoom"
+                    href="https://github.com/Jignesh220"
+                    target="_blank"
+                  >
+                    <i alt="GitHub logo" class="bi bi-github" />
+                  </a>
+                </div>
+                <a className="btn col-10 col-md-6 mx-auto button-rounded p-1 py-2 mt-4 border-0 zoom1 text-light shadow-lg">
+                  Resume
+                </a>
+                <br />
+                <a className="btn col-10 col-md-6 mx-auto button-rounded p-1 py-2 mt-4 border-0 zoom1 text-light shadow-lg" href="#project">
+                  Projects
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
