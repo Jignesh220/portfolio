09f855dfabb3693ed9c6ce05f1538ee044b91b25
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2021-12-28 12:58:59 +0530

    firebase

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -37,7 +37,7 @@ export default function Intro() {
                     <i alt="GitHub logo" class="bi bi-github" />
                   </a>
                 </div>
-                <a className="btn col-10 col-md-6 mx-auto button-rounded p-1 py-2 mt-4 border-0 zoom1 text-light shadow-lg">
+                <a className="btn col-10 col-md-6 mx-auto button-rounded p-1 py-2 mt-4 border-0 zoom1 text-light shadow-lg" href="https://firebasestorage.googleapis.com/v0/b/jignesh-baria.appspot.com/o/JigneshBaria_Resume_simform.pdf?alt=media&token=0129c50b-dc74-46a9-befe-345921e6c962">
                   Resume
                 </a>
                 <br />
