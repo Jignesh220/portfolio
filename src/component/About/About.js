0f96f200144716329552b4eeef3a4a18aaf774a1
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2022-08-15 16:49:08 +0530

    final update

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -49,7 +49,7 @@ export default function About() {
                   <div className="mt-3 row ms-1">
                     Here are a few technologies I’ve been working with recently:
                     <div className="mt-2">
-                      <div className="col-12 col-lg-6 col-md-6 col-sm-12 mx-auto mono-text">
+                      <span className="col-12 col-lg-6 col-md-6 col-sm-12 mx-auto mono-text">
                         <ul>
                           <li>Javascript(ES 6+)</li>
                           <li>Nodejs</li>
@@ -57,15 +57,15 @@ export default function About() {
                           <li>Firebase</li>
                           <li>Rest API</li>
                         </ul>
-                      </div>
-                      <div className="col-12 col-lg-6 col-md-6 col-sm-12 mx-auto mono-text">
+                      </span>
+                      <span className="col-12 col-lg-6 col-md-6 col-sm-12 mx-auto mono-text">
                         <ul>
                           <li>Java</li>
                           <li>Android Studio</li>
                           <li>HTML</li>
                           <li>CSS/Bootstrap</li>
                         </ul>
-                      </div>
+                      </span>
                     </div>
                   </div>
                 </div>
